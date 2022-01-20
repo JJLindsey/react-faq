9ceb8120f80be4172b0cadc98140201edcc0fd92
JJLindsey <jlindsey010@gmail.com>
2022-01-20 12:15:35 -0800

    add accordian and styling

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,22 +1,74 @@
 import React from "react"
+import { Container } from "react-bootstrap"
 import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 
 function FAQ() {
-    return(
-        <Accordion>
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>What forms of payment do you accept?</Accordion.Header>
-                <Accordion.Body>
-                Payment Info
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>What is your cancellation policy?</Accordion.Header>
-                <Accordion.Body>
-                Cancellation info
-                </Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
+    
+    return (
+        <Container fluid>
+            <Accordion className="accordian">
+            <h1 id="faq-title">Frequently Asked Questions</h1>
+                <Card id="faq-card">
+                    <Card.Body>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>What forms of payment do you accept?</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Card.Body>
+                    <Card.Body>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>What is your cancellation policy?</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Card.Body>
+                    <Card.Body>
+                    <Accordion.Item eventKey="3">
+                        <Accordion.Header>How do I contact someone, if I have a question?</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Card.Body>
+                    <Card.Body>
+                    <Accordion.Item eventKey="4">
+                        <Accordion.Header>How do I enroll?</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Card.Body>
+                </Card>
+            </Accordion>
+        </Container>
     )
 }
 export default FAQ
